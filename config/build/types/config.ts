0d11a4ec1686864addb41a62e2ad885f2b1959e3
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2023-10-27 16:30:47 +0300

    чистка комментов

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -8,11 +8,6 @@ export interface BuildPaths {
 
 export interface BuildOptions {
   mode: BuildMode;
-
-  // тут будут различные пути (до входной точки, до папки куда делается сборка,
-  // до HTML и т.д.)
-  // Это будет выглядить так: мы будем принимать их из вне. И там, откуда они будут
-  // поступать, мы будем их конфигурировать по условию
   paths: BuildPaths;
   isDev: boolean;
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -14,11 +14,6 @@ const isDev = mode === "development";
 
 const config: webpack.Configuration = buildWebpackConfig({
   mode: "development",
-
-  // тут будут различные пути (до входной точки, до папки куда делается сборка,
-  // до HTML и т.д.)
-  // Это будет выглядить так: мы будем принимать их из вне. И там, откуда они будут
-  // поступать, мы будем их конфигурировать по условию
   paths,
   isDev,
 });
