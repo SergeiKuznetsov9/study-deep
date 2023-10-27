1e291f43172718c6c6735b166c52776311a7a8be
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2023-10-27 15:12:27 +0300

    1-1 Перевод вебпак конфига на TS

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
 
-module.exports = {
+const config: webpack.Configuration = {
   mode: "development",
 
   entry: path.resolve(__dirname, "src", "index.ts"),
@@ -34,3 +34,5 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
 };
+
+export default config;
