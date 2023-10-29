ad2384720d12f534da5d94d0f463f9bc0e6999ed
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2023-10-29 22:52:29 +0300

    1-6 Добавление роутинга и ленивой подгрузки компонентов

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
-import styles from "./App.module.scss";
-import "./index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
+import { Suspense } from "react";
 
-export const App = () => <div className={styles.greetings}>Hello world</div>;
+export const App = () => (
+  <div>
+    <Link to={"/"}>ГЛАВНАЯ</Link>
+    <Link to={"/about"}>О НАС</Link>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path={"/"} element={<MainPageAsync />} />
+        <Route path={"/about"} element={<AboutPageAsync />} />
+      </Routes>
+    </Suspense>
+  </div>
+);
