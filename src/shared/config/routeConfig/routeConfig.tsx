a35628fd2535dfe96dde2727c30185a8d0306380
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2023-11-01 18:24:47 +0300

    отчистка комментов

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -7,16 +7,11 @@ export enum AppRoutes {
   ABOUT = "about",
 }
 
-// Здесь мы просто укажем путь до соответсвующего компонента до каждого маршрута
-// Record<AppRoutes, string> означает объект, содержащий в качестве ключей составляющие
-// enum AppRoutes, а в качестве значений - строки (сам путь)
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: "/",
   [AppRoutes.ABOUT]: "/about",
 };
 
-// далее объявляем сами роуты (маршруты, соответсвующие им компоненты)
-// RouteProps - это тип из самого реакт-рутер-дом - это тип просов комопнента Route
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MAIN]: {
     path: RoutePath.main,
