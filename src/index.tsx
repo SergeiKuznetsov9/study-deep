3934ed4a4c8144274c532238d364ac90116500b5
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2023-11-21 17:55:13 +0300

    4-30 добавлен редакс-тулкит, реализован счетчик, использован реселект

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,20 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 import { ErrorBoundary } from "app/providers/ErrorBoundary";
+import { StoreProvider } from "app/providers/StoreProvider";
 import "./app/styles/index.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </BrowserRouter>
+  <StoreProvider>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </StoreProvider>
 );
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,15 @@
+import { Counter } from "entities/Counter";
 import { useTranslation } from "react-i18next";
 
 const MainPage = () => {
   const { t } = useTranslation("main");
 
-  return <div>{t("Главная страница")}</div>;
+  return (
+    <div>
+      <div>{t("Главная страница")}</div>
+      <Counter />
+    </div>
+  );
 };
 
 export default MainPage;
