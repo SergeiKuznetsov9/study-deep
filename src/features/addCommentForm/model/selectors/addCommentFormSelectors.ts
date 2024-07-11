fe180fdca1cc4e45eb55def4c91dac6ad6954c41
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-07-11 16:04:08 +0300

    9-58 устранение консольного предупреждения A component is changing an uncontrolled input to be controlled

--- src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
+++ src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
@@ -1,6 +1,8 @@
 import { StateSchema } from "app/providers/StoreProvider";
 
 export const getAddCommentFormText = (state: StateSchema) =>
-  state.addCommentForm?.text;
+  // Это поле не проинициализировано пустой строкой по умолчанию, поэтому и возникает
+  // предупреждение. Проинициализируем:
+  state.addCommentForm?.text ?? "";
 export const getAddCommentFormError = (state: StateSchema) =>
   state.addCommentForm?.error;
