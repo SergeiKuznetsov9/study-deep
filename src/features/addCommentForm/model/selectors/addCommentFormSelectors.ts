ff1815f1ffff2ca50e7616152773abe68cf7a07e
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-07-11 16:04:40 +0300

    отчистка комитов

--- src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
+++ src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
@@ -1,8 +1,6 @@
 import { StateSchema } from "app/providers/StoreProvider";
 
 export const getAddCommentFormText = (state: StateSchema) =>
-  // Это поле не проинициализировано пустой строкой по умолчанию, поэтому и возникает
-  // предупреждение. Проинициализируем:
   state.addCommentForm?.text ?? "";
 export const getAddCommentFormError = (state: StateSchema) =>
   state.addCommentForm?.error;
