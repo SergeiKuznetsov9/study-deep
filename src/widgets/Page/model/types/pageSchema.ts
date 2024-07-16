74953b83bf00799ed04d2be70d35b0b7510d25de
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-07-16 18:09:53 +0300

    отчистка комментов

--- src/widgets/Page/model/types/pageSchema.ts
+++ src/widgets/Page/model/types/pageSchema.ts
@@ -1,5 +1,3 @@
-// Адрес страницы, позиция скролла
-// Планируется сохраниять по ключу страницы позицию скролла в момент его прокрутки
 export type ScrollSchema = Record<string, number>;
 
 export type PageSchema = {
