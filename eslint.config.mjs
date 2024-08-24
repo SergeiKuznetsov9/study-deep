a4228534809b3097a5d3e0f789aed7660c8d3374
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2024-08-24 16:45:07 +0300

    добавить настройки линтера

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,6 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-// Здесь неважно что написать wayfarer или что то другое. Важно, что именно под этим
-// именем будет доступен плагин
 import wayfarer from 'eslint-plugin-wayfarer-plugin'
 
 export default [
@@ -19,6 +17,7 @@ export default [
     rules: {
       "react/react-in-jsx-scope": "off",
       "wayfarer/path-checker": "warn",
+      
     },
   },
 ];
