7f225e370b0f5c6fb9286d03ad75f2891f79e1dc
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2024-09-07 13:22:40 +0300

    отчистка комментов

--- src/features/articleRecommendationsList/api/ArticleRecommendationsApi.ts
+++ src/features/articleRecommendationsList/api/ArticleRecommendationsApi.ts
@@ -3,7 +3,6 @@ import { rtkApi } from "shared/api/rtkApi";
 
 const recommendationsApi = rtkApi.injectEndpoints({
   endpoints: (build) => ({
-    // первый тип в дженерике - что вернется, второй - аргумент
     getArticleRecommendationsList: build.query<Article[], number>({
       query: (limit) => ({
         url: "/articles",
