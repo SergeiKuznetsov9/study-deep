caf45918f752e97b3618ef51f851b053a832ae6a
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-09-23 18:19:56 +0300

    12-85 добавить сборщик vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgrPlugin({
+      include: "**/*.svg",
+      svgrOptions: {
+        exportType: "default",
+      },
+    }),
+  ],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  define: {
+    __IS_DEV__: JSON.stringify(true),
+    __API__: JSON.stringify("http://localhost:8000"),
+  },
+});
