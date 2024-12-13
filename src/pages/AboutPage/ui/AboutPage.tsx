b47ab7bd1efd2ab51a88dc3bc23c1bd9f45e7b8f
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-12-13 17:07:23 +0300

    редактирование начальных страниц

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,17 @@
 import { useTranslation } from "react-i18next";
 import { Page } from "@/widgets/Page";
+import { Card } from "@/shared/ui/Card";
+import { Text } from "@/shared/ui/Text";
 
 const AboutPage = () => {
   const { t } = useTranslation();
-  return <Page>{t("О сайте")}</Page>;
+  return (
+    <Page>
+      <Card padding="24" max border="partial">
+        <Text title={t("О сайте")} />
+      </Card>
+    </Page>
+  );
 };
 
 export default AboutPage;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,27 @@
 import { useTranslation } from "react-i18next";
 
 import { Page } from "@/widgets/Page";
+import { Card } from "@/shared/ui/Card";
+import { Text } from "@/shared/ui/Text";
+import { VStack } from "@/shared/ui/Stack";
 
 const MainPage = () => {
   const { t } = useTranslation();
 
   return (
     <Page>
-      <div>{t("Главная")}</div>
+      <Card padding="24" max border="partial">
+        <VStack gap="24">
+          <Text title={t("Главная")} />
+          <VStack gap="8">
+            <Text text={t("Для авторизации используйте:")} />
+            <VStack gap="4">
+              <Text text={t("Логин: admin")} />
+              <Text text={t("Пароль: 123")} />
+            </VStack>
+          </VStack>
+        </VStack>
+      </Card>
     </Page>
   );
 };
